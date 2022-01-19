91a561b24e6cad1752077474c115eff7ec7d062c
qc420 <qc420@imperial.ac.uk>
2022-01-19 23:26:36 +0000

    Initial implementation of Fibonacci package.

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="fibonacci", #name of the package
+    version="0.1", #not for public realise
+    packages=find_packages() #it tells where the packages are
+)
